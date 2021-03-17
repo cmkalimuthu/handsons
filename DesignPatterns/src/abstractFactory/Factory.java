db957a8533a4e58e59571fd88c461d8513b8195b
cmkalimuthu <cmkalimuthu@gmail.com>
2021-03-17 18:48:14 +0530

    Day 1 hands on design patterns

---
+++ DesignPatterns/src/abstractFactory/Factory.java
@@ -0,0 +1,6 @@
+package abstractFactory;
+
+public interface  Factory {
+   void producecomponents();
+    
+}
---
+++ DesignPatterns/src/singleton/Main.java
@@ -0,0 +1,12 @@
+package singleton;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		DBconn db=DBconn.getInstance();
+		System.out.println(db);
+		
+	
+     }
+	
+}
